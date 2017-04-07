a049dc47641b0d7fa890ad67aed0831d6f7c8e74
Bergur Hallgrímsson <bergurh@tm.is>
2017-04-07 17:01:53 +0000

    Write og read

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 const app = angular.module('baeting',['firebase']);
 app.config(() => {
   const config = {
-    apiKey: "AIzaSyD29Ud1WA6JXh3U3ZQJXTEmpy0uHXD_oB8",
-    authDomain: "baetingarad.firebaseapp.com",
-    databaseURL: "https://baetingarad.firebaseio.com",
-    projectId: "baetingarad",
-    storageBucket: "baetingarad.appspot.com",
-    messagingSenderId: "74254762585"
+    apiKey: "AIzaSyDHGJT4If7Fw8NmfqDQWfQdv_dN8z50nP0",
+    authDomain: "hallakaffi-2d2fb.firebaseapp.com",
+    databaseURL: "https://hallakaffi-2d2fb.firebaseio.com",
+    projectId: "hallakaffi-2d2fb",
+    storageBucket: "hallakaffi-2d2fb.appspot.com",
+    messagingSenderId: "440383505953"
   }
   firebase.initializeApp(config);
 });
@@ -15,6 +15,31 @@ app.config(() => {
 app.factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
   return $firebaseAuth();
 }]);
+
+app.factory('Candy', ['$firebaseArray','$firebaseObject', function($firebaseArray, $firebaseObject){
+  const ref = firebase.database().ref('candy');
+  return $firebaseArray(ref);
+}])
+
+app.factory('Buy', ['$firebaseArray', function($firebaseArray){
+  const ref = firebase.database().ref('buy');  
+  
+  const buy = $firebaseArray.$extend({
+    $$added: function(snap) {
+      console.log(snap.val()); 
+      return snap.val();
+    }    
+  });
+
+  return buy(ref);
+  //return $firebaseArray(ref);
+}])
+
+app.factory('Sell', ['$firebaseArray', function($firebaseArray){
+  const ref = firebase.database().ref('sell');  
+  return $firebaseArray(ref);
+}])
+
 
 app.controller('HomeCtrl', ['$scope', 'Auth', function($scope, Auth) {
   $scope.login = function() {
@@ -26,5 +51,46 @@ app.controller('HomeCtrl', ['$scope', 'Auth', function($scope, Auth) {
       }
       console.log(error);
     });
+  }
+}]);
+
+app.controller('CandyCtrl', ['$scope', 'Candy', function($scope, Candy) {
+  $scope.candyList = Candy;
+  
+  $scope.insert = function() {
+    $scope.candyList.$add({
+      name: $scope.name,
+      price: $scope.price
+    })  
+  }
+}]);
+
+app.controller('BuyCtrl', ['$scope', 'Buy','Candy', function($scope, Buy, Candy) {   
+  $scope.buyList = Buy;  
+  $scope.candyList = Candy;
+  $scope.selectedCandy = null;
+
+  $scope.getCandy = function(key) {
+    const ref = firebase.database().ref('candy');
+    return 'test';
+  }
+  
+  $scope.insert = function() {
+    console.log($scope.selectedCandy);
+    $scope.buyList.$add({
+      count: $scope.count,
+      candy: $scope.selectedCandy.$id,
+      date: new Date().getTime()
+    })  
+  }
+}]);
+
+app.controller('SellCtrl', ['$scope', 'Sell','Candy', function($scope, Sell, Candy) {   
+  $scope.sellList = Sell;  
+  $scope.candyList = Candy;
+  $scope.selectedCandy = null;
+
+  $scope.sell = function(id) {
+    console.log(id);
   }
 }]);
